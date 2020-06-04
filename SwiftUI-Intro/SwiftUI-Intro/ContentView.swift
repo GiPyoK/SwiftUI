a6207cf17191ca31c304e10abc20872833aa7b69
Gi Pyo Kim <gipyok@gmail.com>
2020-06-03 22:33:35 -0700

    Added Image

--- SwiftUI-Intro/SwiftUI-Intro/ContentView.swift
+++ SwiftUI-Intro/SwiftUI-Intro/ContentView.swift
@@ -14,8 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         List(self.hikes, id: \.name) { hike in
-            Text(hike.name)
-                .font(.title)
+            HikeCell(hike: hike)
         }
     }
 }
@@ -23,5 +22,25 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HikeCell: View {
+    
+    let hike: Hike
+    
+    var body: some View {
+        HStack {
+            Image(hike.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+            
+            VStack(alignment: .leading) {
+                Text(hike.name)
+                Text(String(format: "%.2f miles", hike.miles))
+            }
+            
+        }
     }
 }
