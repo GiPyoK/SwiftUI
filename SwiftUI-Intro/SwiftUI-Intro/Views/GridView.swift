3f5bc265494e75fecd0e0834c1f5fbbd857b0365
Gi Pyo Kim <gipyok@gmail.com>
2020-06-04 20:47:43 -0700

    Grid View initial commit

---
+++ SwiftUI-Intro/SwiftUI-Intro/Views/GridView.swift
@@ -0,0 +1,21 @@
+//
+//  GridView.swift
+//  SwiftUI-Intro
+//
+//  Created by Gi Pyo Kim on 6/4/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
