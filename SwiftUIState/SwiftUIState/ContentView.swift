5f83e660ba581d4e8ddcbae22915cbe99b6a6910
Gi Pyo Kim <gipyok@gmail.com>
2020-06-10 13:32:34 -0700

    State with list

--- SwiftUIState/SwiftUIState/ContentView.swift
+++ SwiftUIState/SwiftUIState/ContentView.swift
@@ -9,21 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks = [Task]()
     
-    @State var name: String = "John"
+    private func addTask() {
+        self.tasks.append(Task(name: "Fix the car"))
+    }
     
     var body: some View {
-        VStack {
-            Text(name).font(.largeTitle)
+        List {
+            Button(action: addTask) {
+                Text("Add Task")
+            }
             
-            Button(action: {
-                self.name = "Gi Pyo"
-            }) {
-                Text("Change Name")
+            ForEach(tasks) { task in
+                Text(task.name)
             }
         }
     }
 }
+
+//struct ContentView: View {
+//
+//    @State var name: String = "John"
+//
+//    var body: some View {
+//        VStack {
+//            Text(name).font(.largeTitle)
+//
+//            Button(action: {
+//                self.name = "Gi Pyo"
+//            }) {
+//                Text("Change Name")
+//            }
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SwiftUIState/SwiftUIState/Models/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  SwiftUIState
+//
+//  Created by Gi Pyo Kim on 6/10/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct Task: Identifiable {
+    let id = UUID()
+    let name: String
+}
