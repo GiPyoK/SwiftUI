79b58d80de54be6fe89d66d723a4c662427abff1
Gi Pyo Kim <gipyok@gmail.com>
2020-06-10 15:32:03 -0700

    Observable Object timer initial commit

---
+++ SwiftUI-ObservableObject/SwiftUI-ObservableObject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-ObservableObject
+//
+//  Created by Gi Pyo Kim on 6/10/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
