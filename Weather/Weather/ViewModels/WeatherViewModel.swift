32c602c910755f8eac9d4b0b4015f4ba32c10de7
Gi Pyo Kim <gipyok@gmail.com>
2020-06-15 13:00:51 -0700

    Created WeatherViewModel to fetch weather through WeatherService

---
+++ Weather/Weather/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  WeatherViewModel.swift
+//  Weather
+//
+//  Created by Gi Pyo Kim on 6/15/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class WeatherViewModel: ObservableObject {
+        
+    private var weatherService: WeatherService
+    
+    @Published var weather = Weather()
+    
+    var temperature: String {
+        if let temp = self.weather.temp {
+            return String(format: "%.0f", temp)
+        } else {
+            return ""
+        }
+    }
+    
+    var humidity: String {
+        if let humidity = self.weather.humidity {
+            return String(format: "%.0f", humidity)
+        } else {
+            return ""
+        }
+    }
+    
+    var cityName: String = ""
+    
+    init() {
+        self.weatherService = WeatherService()
+    }
+    
+    private func fetchWeather(by city: String) {
+        self.weatherService.getWeather(city: city) { (weather) in
+            if let weather = weather {
+                self.weather = weather
+            }
+        }
+    }
+    
+    func search() {
+        if let city = self.cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            fetchWeather(by: city)
+        }
+    }
+}
