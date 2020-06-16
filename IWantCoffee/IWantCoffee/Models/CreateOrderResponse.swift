8b53535ed262fcb36217187770e047cd28e86020
Gi Pyo Kim <gipyok@gmail.com>
2020-06-16 12:30:21 -0700

    Implemented post request to submit an order

---
+++ IWantCoffee/IWantCoffee/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  IWantCoffee
+//
+//  Created by Gi Pyo Kim on 6/16/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
--- IWantCoffee/IWantCoffee/Services/WebService.swift
+++ IWantCoffee/IWantCoffee/Services/WebService.swift
@@ -12,6 +12,7 @@ class WebService {
     private let baseURL = URL(string: "https://coffe-order.glitch.me")!
     
     func getAllOrders(completion: @escaping ([Order]?) -> ()) {
+        
         let requestURL = self.baseURL.appendingPathComponent("orders")
         
         URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
@@ -25,6 +26,31 @@ class WebService {
             let orders = try? JSONDecoder().decode([Order].self, from: data)
             DispatchQueue.main.async {
                 completion(orders)
+            }
+            
+        }.resume()
+    }
+    
+    func createCoffeeOrder(order: Order,  completion: @escaping (CreateOrderResponse?) -> ()) {
+        
+        let requestURL = baseURL.appendingPathComponent("orders")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(order)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+            DispatchQueue.main.async {
+                completion(createOrderResponse)                
             }
             
         }.resume()
