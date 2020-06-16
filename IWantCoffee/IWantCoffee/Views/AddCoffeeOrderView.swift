da229c20af24f2ffd2b2ded0031413ecd26e41c9
Gi Pyo Kim <gipyok@gmail.com>
2020-06-16 14:04:42 -0700

    Added basic UI for ordering coffee: name, coffee selection, coffee size selection, and placing order button

---
+++ IWantCoffee/IWantCoffee/Views/AddCoffeeOrderView.swift
@@ -0,0 +1,83 @@
+//
+//  AddCoffeeOrderView.swift
+//  IWantCoffee
+//
+//  Created by Gi Pyo Kim on 6/16/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddCoffeeOrderView: View {
+    
+    @ObservedObject private var addCoffeeOrderVM = AddCoffeeOrderViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("INFORMATION").font(.body)) {
+                        TextField("Enter name", text: self.$addCoffeeOrderVM.name)
+                    }
+                    
+                    Section(header: Text("SELECT COFFEE").font(.body)) {
+                        ForEach(addCoffeeOrderVM.coffeeList, id:\.name) { coffee in
+                            CoffeCellView(coffee: coffee, selection: self.$addCoffeeOrderVM.coffeeName)
+                        }
+                    }
+                    
+                    Section(header: Text("SELECT SIZE").font(.body), footer: Text("Total: ")) {
+                        Picker("", selection: self.$addCoffeeOrderVM.size) {
+                            Text("Small").tag("Small")
+                            Text("Medium").tag("Medium")
+                            Text("Large").tag("Large")
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+                
+                HStack {
+                    Button("Place Order") {
+                        
+                    }
+                }.padding(EdgeInsets(top: 12, leading: 100, bottom: 12, trailing: 100))
+                    .background(Color(red: 46/255, green: 204/255, blue: 113/255))
+                    .foregroundColor(Color.white)
+                    .cornerRadius(8)
+                
+            }
+            .navigationBarTitle("Add Order")
+        }
+    }
+}
+
+struct AddCoffeeOrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCoffeeOrderView()
+    }
+}
+
+struct CoffeCellView: View {
+    
+    let coffee: CoffeeViewModel
+    @Binding var selection: String
+    
+    var body: some View {
+        HStack {
+            Image(coffee.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .leading)
+                .cornerRadius(8)
+            
+            Text(coffee.name)
+                .font(.title)
+                .padding(.leading, 20)
+            
+            Spacer()
+            
+            Image(systemName: self.selection == self.coffee.name ? "checkmark" : "")
+            .padding()
+        }.onTapGesture {
+            self.selection = self.coffee.name
+        }
+    }
+}
