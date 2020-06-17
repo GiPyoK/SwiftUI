7588c3d83f0a6a69ad534c82e66a415a5846ec97
Gi Pyo Kim <gipyok@gmail.com>
2020-06-17 15:12:23 -0700

    XCode Preview

---
+++ Xcode-Previews/Xcode-Previews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode-Previews
+//
+//  Created by Gi Pyo Kim on 6/17/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode-Previews/Xcode-Previews/퍋ㅈㄴ/DishCell.swift
@@ -0,0 +1,29 @@
+//
+//  DishCell.swift
+//  Xcode-Previews
+//
+//  Created by Gi Pyo Kim on 6/17/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct DishCell: View {
+    var body: some View {
+        
+        Text("DishCell")
+        
+    }
+}
+
+
+#if DEBUG
+
+struct DishCell_Preview: PreviewProvider {
+    static var previews: some View {
+        DishCell()
+    }
+}
+
+#endif
