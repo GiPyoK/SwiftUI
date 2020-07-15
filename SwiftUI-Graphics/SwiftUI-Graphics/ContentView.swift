38bde16c6242772721a96f372fbe0b876ce57db3
Gi Pyo Kim <gipyok@gmail.com>
2020-07-14 20:24:18 -0700

    Graphics initial commit

---
+++ SwiftUI-Graphics/SwiftUI-Graphics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Graphics
+//
+//  Created by Gi Pyo Kim on 7/14/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
