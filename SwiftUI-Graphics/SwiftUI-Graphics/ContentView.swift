d073cbe573f6165aa2b6610307fdbcb342289951
Gi Pyo Kim <gipyok@gmail.com>
2020-07-14 20:27:23 -0700

    Draw basic shapes with SwiftUI

--- SwiftUI-Graphics/SwiftUI-Graphics/ContentView.swift
+++ SwiftUI-Graphics/SwiftUI-Graphics/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack{
+            Circle()
+                .fill(Color.red)
+                .frame(width: 200, height: 200)
+            
+            Circle()
+                .stroke(Color.green, lineWidth: 20)
+            
+            Rectangle()
+                .fill(Color.yellow)
+                .frame(width: 100, height: 200)
+        }
+        
+        
     }
 }
 
