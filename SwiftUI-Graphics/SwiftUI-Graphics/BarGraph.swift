f0c952d95910d78bffd07f0224d9a77413309a83
Gi Pyo Kim <gipyok@gmail.com>
2020-07-16 22:46:49 -0700

    Created a graph from data given from Report class

--- SwiftUI-Graphics/SwiftUI-Graphics/BarGraph.swift
+++ SwiftUI-Graphics/SwiftUI-Graphics/BarGraph.swift
@@ -10,33 +10,58 @@ import Foundation
 import SwiftUI
 
 struct BarGraph: View {
+    
+    let reports: [Report]
+    
     var body: some View {
         VStack {
             HStack(alignment: .lastTextBaseline) {
-                Rectangle()
-                    .fill(Color.orange)
-                    .frame(width: 100, height: 180)
-                    .padding(2)
+//                Rectangle()
+//                    .fill(Color.orange)
+//                    .frame(width: 100, height: 180)
+//                    .padding(2)
+//
+//                Rectangle()
+//                    .fill(Color.green)
+//                    .frame(width: 100, height: 240)
+//                    .padding(2)
+//
+//                Rectangle()
+//                    .fill(Color.blue)
+//                    .frame(width: 100, height: 360)
+//                    .padding(2)
+//
+//                Spacer()
                 
-                Rectangle()
-                    .fill(Color.green)
-                    .frame(width: 100, height: 240)
-                    .padding(2)
-                
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 100, height: 360)
-                    .padding(2)
-                
-                Spacer()
+                ForEach(self.reports, id: \.year) { report in
+                    BarView(report: report)
+                }
             }
         }
     }
 }
 
+struct BarView: View {
+    let report: Report
+    
+    var body: some View {
+        let value = report.revenue / 500
+        let yValue = Swift.min(value * 20, 500)
+        
+        return VStack {
+            Text(String(format: "$%.2f", report.revenue))
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 100, height: CGFloat(yValue))
+            
+            Text(report.year)
+        }
+    }
+}
 
 struct BarGraph_Previews: PreviewProvider {
     static var previews: some View {
-        BarGraph()
+        BarGraph(reports: Report.all())
     }
 }
--- SwiftUI-Graphics/SwiftUI-Graphics/ContentView.swift
+++ SwiftUI-Graphics/SwiftUI-Graphics/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Circle()
+        BarGraph(reports: Report.all())
 // Basic Shapes
 //        VStack{
 //            Circle()
