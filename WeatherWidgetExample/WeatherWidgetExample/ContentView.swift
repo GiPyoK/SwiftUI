643e158a79e66e0bd80dcaf562283b9fae05df43
Gi Pyo Kim <gipyok@gmail.com>
2020-11-23 18:58:21 -0800

    Weather Widget example created

---
+++ WeatherWidgetExample/WeatherWidgetExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherWidgetExample
+//
+//  Created by Gi Pyo Kim on 11/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherWidgetExample/WeatherWidgetExample/WeatherWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetExampleApp.swift
+//  WeatherWidgetExample
+//
+//  Created by Gi Pyo Kim on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
